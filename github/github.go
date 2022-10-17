ccdb2e1eb63ab43dbd696941cc3ff9efea51c07a
Manuel de la Peña <mdelapenya@gmail.com>
2022-10-17 11:36:14 +0200

    chore: add an exponential backoff for searching github

--- github/github.go
+++ github/github.go
@@ -1,6 +1,10 @@
 package github
 
 import (
+	"fmt"
+	"time"
+
+	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/mdelapenya/github-metrics/http"
 )
 
@@ -12,6 +16,26 @@ func Labels(owner string, repository string) ([]byte, error) {
 	return repoGet("labels", owner, repository)
 }
 
+// getExponentialBackOff returns a preconfigured exponential backoff instance
+func getExponentialBackOff(elapsedTime time.Duration) *backoff.ExponentialBackOff {
+	var (
+		initialInterval     = 500 * time.Millisecond
+		randomizationFactor = 0.5
+		multiplier          = 2.0
+		maxInterval         = 5 * time.Second
+		maxElapsedTime      = elapsedTime
+	)
+
+	exp := backoff.NewExponentialBackOff()
+	exp.InitialInterval = initialInterval
+	exp.RandomizationFactor = randomizationFactor
+	exp.Multiplier = multiplier
+	exp.MaxInterval = maxInterval
+	exp.MaxElapsedTime = maxElapsedTime
+
+	return exp
+}
+
 func repoGet(resource string, owner string, repository string) ([]byte, error) {
 	req := http.Request{
 		URL: "https://api.github.com/repos/" + owner + "/" + repository + "/" + resource,
@@ -21,12 +45,33 @@ func repoGet(resource string, owner string, repository string) ([]byte, error) {
 }
 
 func search(owner string, repository string, queryString string) ([]byte, error) {
-	req := http.Request{
-		URL: "https://api.github.com/search/issues?q=repo:" + owner + "/" + repository + "+" + queryString,
-		Headers: map[string]string{
-			"User-Agent": "request",
-		},
+	exp := getExponentialBackOff(time.Minute * 1)
+
+	var bytes []byte
+	retryCount := 0
+	searchFn := func() error {
+		req := http.Request{
+			URL: "https://api.github.com/search/issues?q=repo:" + owner + "/" + repository + "+" + queryString,
+			Headers: map[string]string{
+				"User-Agent": "request",
+			},
+		}
+
+		b, err := http.Get(req)
+		if err != nil {
+			retryCount++
+			fmt.Printf(">>> [%d][%s] error while processing the Github search for \"%s\". Retrying: %v\n", retryCount, exp.GetElapsedTime(), queryString, err)
+			return err
+		}
+
+		bytes = b
+		return nil
 	}
 
-	return http.Get(req)
+	err := backoff.Retry(searchFn, exp)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return bytes, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/spf13/cobra v1.6.0
 
 require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
